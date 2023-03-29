a2589fa021dcde40014d8ade00cffde186a1cc6f
Aaron <aatran2021@gmail.com>
2023-03-29 15:41:26 -0700

    Unmonolith pub/sub attendees

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -1,5 +1,5 @@
 from django.http import JsonResponse
-from .models import Attendee, ConferenceVO
+from .models import Attendee, ConferenceVO, AccountVO
 
 # from events.models import Conference
 from common.json import ModelEncoder
@@ -33,6 +33,13 @@ class AttendeeDetailEncoder(ModelEncoder):
     encoders = {
         "conference": ConferenceVODetailEncoder(),
     }
+
+    def get_extra_data(self, o):
+        count = AccountVO.objects.filter(email=o.email).count()
+        if count > 0:
+            return {"has_account": True}
+        else:
+            return {"has_account": False}
 
 
 @require_http_methods(["GET", "POST"])
--- monolith/presentations/api_views.py
+++ monolith/presentations/api_views.py
@@ -4,6 +4,7 @@ from events.models import Conference
 from common.json import ModelEncoder
 from django.views.decorators.http import require_http_methods
 import json
+import pika
 
 
 class ConferenceListEncoder(ModelEncoder):
@@ -148,3 +149,61 @@ def api_show_presentation(request, id):
             encoder=PresentationDetailEncoder,
             safe=False,
         )
+
+
+@require_http_methods(["PUT"])
+def api_approve_presentation(request, pk):
+    presentation = Presentation.objects.get(id=pk)
+    presentation.approve()
+
+    parameters = pika.ConnectionParameters(host="rabbitmq")
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue="presentation_approvals")
+    channel.basic_publish(
+        exchange="",
+        routing_key="presentation_approvals",
+        body=json.dumps(
+            {
+                "presenter_name": presentation.presenter_name,
+                "presenter_email": presentation.presenter_email,
+                "title": presentation.title,
+            }
+        ),
+    )
+    connection.close()
+
+    return JsonResponse(
+        presentation,
+        encoder=PresentationDetailEncoder,
+        safe=False,
+    )
+
+
+@require_http_methods(["PUT"])
+def api_reject_presentation(request, pk):
+    presentation = Presentation.objects.get(id=pk)
+    presentation.reject()
+
+    parameters = pika.ConnectionParameters(host="rabbitmq")
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_declare(queue="presentation_rejections")
+    channel.basic_publish(
+        exchange="",
+        routing_key="presentation_rejections",
+        body=json.dumps(
+            {
+                "presenter_name": presentation.presenter_name,
+                "presenter_email": presentation.presenter_email,
+                "title": presentation.title,
+            }
+        ),
+    )
+    connection.close()
+
+    return JsonResponse(
+        presentation,
+        encoder=PresentationDetailEncoder,
+        safe=False,
+    )
